6fb609f116e29192e86a64c71bac02f42ab246ab
Martin Frost Thorhauge <MartinFrost.Thorhauge@ufst.dk>
2021-10-14 14:21:30 +0200

    First draft of the plant_route_parcel page is done.

--- lib/screens/route/plan_route_parcel.dart
+++ lib/screens/route/plan_route_parcel.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
+enum PriceAdjustment{markup, discount}
+
 class PlanRouteParcel extends StatelessWidget {
   const PlanRouteParcel({Key? key}) : super(key: key);
+  final PriceAdjustment? _adjustment = PriceAdjustment.markup;
 
   @override
   Widget build(BuildContext context) {
@@ -30,6 +33,100 @@ class PlanRouteParcel extends StatelessWidget {
                   child:  TextField(
                     decoration: InputDecoration(
                         border: OutlineInputBorder(), hintText: "Add a number"),
+                  ),
+                ),
+              ),
+            ]),
+            //TODO: Need to connect with backend endpoint
+            Row( children: const [
+              Text("Category"),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: SizedBox(
+                  width: 300.0,
+                  child:  TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(), hintText: "- Select a category - "),
+                  ),
+                ),
+              ),
+            ]),
+            //TODO: Fix radio buttons for price adjustment.
+            /*Row( children: [
+              const Text("Price adjustment"),
+              ListTile(
+                title: const Text("Markup"),
+                leading: Radio<PriceAdjustment>(
+                  value: PriceAdjustment.markup,
+                  groupValue: _adjustment,
+                  onChanged: (PriceAdjustment? value) {
+                  },
+                ),
+              ),
+              ListTile(
+                title: const Text("Discount"),
+                leading: Radio<PriceAdjustment>(
+                  value: PriceAdjustment.discount,
+                  groupValue: _adjustment,
+                  onChanged: (PriceAdjustment? value) {
+                  },
+                ),
+              ),
+            ]),
+            */Row( children: const [
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: SizedBox(
+                  width: 300.0,
+                  child:  TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(), hintText: "Add a number"),
+                  ),
+                ),
+              ),
+            ]),
+            Row( children: const [
+              Text("Weight"),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: SizedBox(
+                  width: 300.0,
+                  child:  TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(), hintText: "Grams"),
+                  ),
+                ),
+              ),
+            ]),
+            Row( children: const [
+              Text("Size"),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: SizedBox(
+                  width: 300.0,
+                  child:  TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(), hintText: "Length"),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: SizedBox(
+                  width: 300.0,
+                  child:  TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(), hintText: "Width"),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: SizedBox(
+                  width: 300.0,
+                  child:  TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(), hintText: "Height"),
                   ),
                 ),
               ),
