dd0acfc3e05452f2c6c5bbf6acd711bfe3796352
Martin Frost Thorhauge <MartinFrost.Thorhauge@ufst.dk>
2021-10-14 15:56:45 +0200

    Created route_suggestions.dart and used them in the plan_route_choices.dart.

---
+++ lib/model/route_suggestions.dart
@@ -0,0 +1,8 @@
+class RouteSuggestion {
+  final String origin;
+  final String destination;
+  final String duration;
+  final String cost;
+
+  RouteSuggestion(this.origin, this.destination, this.duration, this.cost);
+}
