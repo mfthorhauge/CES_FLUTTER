d43bed9e13aea84cfd1ffa01314f2cba8f4c47bd
Martin Frost Thorhauge <MartinFrost.Thorhauge@ufst.dk>
2021-10-15 01:17:19 +0200

    created getters for in memory objects

--- lib/provider/order_service.dart
+++ lib/provider/order_service.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:telco_web_client/model/address.dart';
 import 'package:telco_web_client/model/customer.dart';
 import 'package:telco_web_client/model/parcel.dart';
 import 'package:telco_web_client/model/route_suggestion.dart';
@@ -9,8 +10,18 @@ import 'package:telco_web_client/model/route_suggestion.dart';
 class OrderService with ChangeNotifier {
   Customer customer = Customer(0, "", "", "", "", "");
   Parcel parcel = Parcel(0, "", 0, 0, 0, 0, 0);
+  Address origin = Address("", "", "", "");
+  Address destination = Address("", "", "", "");
   Future<RouteSuggestion>? cheapestRouteSuggestion;
   Future<RouteSuggestion>? fastestRouteSuggestion;
+
+  Customer get getCustomer => customer;
+
+  Parcel get getParcel => parcel;
+
+  Address get getOrigins => origin;
+
+  Address get getDestination => destination;
 
   void setCustomerId(String newValue) {
     customer.id = int.tryParse(newValue) ?? 0;
