c17de4d792270faa912df6411dec5cb045ea00b7
Martin Frost Thorhauge <MartinFrost.Thorhauge@ufst.dk>
2021-10-14 14:55:22 +0200

    Initial setup of plan_route_choices.dart and adding navigation to main.dart

---
+++ lib/screens/route/plan_route_choices.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+
+class PlanRouteChoices extends StatelessWidget {
+  const PlanRouteChoices({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              children: const [
+                Text("Plan a route"),
+                Spacer(),
+                Text("Step 4 of 5"),
+              ],
+            ),
+            Row(children: const [
+              Text("Origin"),
+              Spacer(),
+              Text("Destination")
+            ]),
+            Row(children: const [
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: SizedBox(
+                  width: 300.0,
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Sender",
+                        labelText: "Sender"),
+                  ),
+                ),
+              ),
+              Spacer(),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: SizedBox(
+                  width: 300.0,
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Receiver",
+                        labelText: "Receiver"),
+                  ),
+                ),
+              ),
+            ]),
+            Row(children: const [
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: SizedBox(
+                  width: 300.0,
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Address",
+                        labelText: "Address"),
+                  ),
+                ),
+              ),
+              Spacer(),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: SizedBox(
+                  width: 300.0,
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Address",
+                        labelText: "Address"),
+                  ),
+                ),
+              ),
+            ]),
+            Row(children: const [
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: SizedBox(
+                  width: 300.0,
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Postcode",
+                        labelText: "Postcode"),
+                  ),
+                ),
+              ),
+              Spacer(),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: SizedBox(
+                  width: 300.0,
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Postcode",
+                        labelText: "Postcode"),
+                  ),
+                ),
+              ),
+            ]),
+            //TODO: These two boxes should be dropdown menues based on the city entities.
+            Row(children: const [
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: SizedBox(
+                  width: 300.0,
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "- Select a city -",
+                        labelText: "City"),
+                  ),
+                ),
+              ),
+              Spacer(),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: SizedBox(
+                  width: 300.0,
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "- Select a city -",
+                        labelText: "City"),
+                  ),
+                ),
+              ),
+            ]),
+            //TODO: Same as billing address import for information fields in form. Needs extra endpoint.
+            Row(children: [
+              const Spacer(),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(
+                      context,
+                    );
+                  },
+                  child: const Text("Back")),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(
+                      context,
+                      '/route2',
+                    );
+                  },
+                  child: const Text("Next Step")),
+            ]),
+          ],
+        ),
+      ),
+    );
+  }
+}
