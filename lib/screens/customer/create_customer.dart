7bca1758fc326cbbd767d1cf5e03c9685da24fe0
gese <gese@netcompany.com>
2021-10-15 11:22:37 +0200

    DEFECT #4 - FS11 – Create new customer: Missing input fields

--- lib/screens/customer/create_customer.dart
+++ lib/screens/customer/create_customer.dart
@@ -9,59 +9,117 @@ class CreateCustomer extends StatelessWidget {
     return Scaffold(
       appBar: CustomAppBar(appBar: AppBar()),
       body: Center(
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        const Text("Create new customer"),
-          Column(children: [
-            const Text("Customer information"),
-            const TextField(
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(), hintText: 'Name'),
-            ),
-            const TextField(
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(), hintText: 'Email'),
-            ),
-            const TextField(
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(), hintText: 'Address'),
-            ),
-            const TextField(
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(), hintText: 'Postcode'),
-            ),
-            DropdownButton(items: <String>[
-              'City1',
-              'City2',
-              'City3',
-            ].map<DropdownMenuItem<String>>((String value) {
-              return DropdownMenuItem<String>(
-                value: value,
-                child: Text(value, style: const TextStyle(color:Colors.black),),
-              );
-            }).toList(),),
-            Row(
-              children: [
-                const Spacer(),
-                TextButton(
-                  child: const Text('CANCEL'),
-                  onPressed: () {
-                    Navigator.pop(
-                      context
-                    );
-                  },
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text("Create new customer"),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                  width: 300,
+                  child: Column(children: [
+                    const Text("Customer information"),
+                    const TextField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(), hintText: 'Name'),
+                    ),
+                    const TextField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(), hintText: 'Email'),
+                    ),
+                    const TextField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(), hintText: 'Address'),
+                    ),
+                    const TextField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(), hintText: 'Postcode'),
+                    ),
+                    DropdownButton(
+                      items: <String>[
+                        'City1',
+                        'City2',
+                        'City3',
+                      ].map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(
+                            value,
+                            style: const TextStyle(color: Colors.black),
+                          ),
+                        );
+                      }).toList(),
+                    ),
+                    const TextField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(), hintText: 'Country'),
+                    ),
+                  ]),
                 ),
-                ElevatedButton(
-                    onPressed: () {
-                      Navigator.pop(
-                          context
-                      );
-                    },
-                    child: const Text("Create New Customer")),
-              ],
-            ),
-          ],)
-        ]),
-      ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                  width: 300,
+                  child: Column(
+                    children: [
+                      const Text("Billing information"),
+                      const TextField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Card holder name'),
+                      ),
+                      Row(
+                        children: const [
+                          SizedBox(
+                            width: 180,
+                            child: TextField(
+                              decoration: InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  hintText: 'Credit Card Number'),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 100,
+                            child: TextField(
+                              decoration: InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  hintText: 'CVV'),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const TextField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Expiration date (YYYY/MM)'),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+          Row(
+            children: [
+              const Spacer(),
+              TextButton(
+                child: const Text('CANCEL'),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text("Create New Customer")),
+            ],
+          ),
+        ],
+      )),
     );
   }
 }
